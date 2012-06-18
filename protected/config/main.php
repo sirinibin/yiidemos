c567551811a453c7922675cf2e1396179be74b9b
Sirin K <sirinibin2006@gmail.com>
2012-06-19 01:49:12 +0530

    first commit

--- protected/config/main.php
+++ protected/config/main.php
@@ -47,7 +47,7 @@ return array(
                                                  //  'ext.giiplus2',    
                                                  ),
 		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			//'ipFilters'=>array('127.0.0.1','::1'),
 		),
 		
 	),
