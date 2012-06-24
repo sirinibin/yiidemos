3d1b343a164fcde6e3e7bd4e4798bc950a9ee250
Sirin K <sirinibin2006@gmail.com>
2012-06-24 13:38:50 +0530

    bug fix

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -27,9 +27,10 @@ class TestController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
+				'actions'=>array('index','view','create','update','GeneratePdf','GenerateExcel','delete'),
 				'users'=>array('*'),
 			),
+                      /*
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update','GeneratePdf','GenerateExcel'),
 				'users'=>array('@'),
@@ -37,7 +38,7 @@ class TestController extends Controller
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
 				'users'=>array('admin'),
-			),
+			),*/
 			array('deny',  // deny all users
 				'users'=>array('*'),
 			),
